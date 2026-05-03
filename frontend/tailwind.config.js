88b41c1a3d5f5fd975d6f51c0ff136fd0c94ce08
momi10 <mohaiman10ahmad@gmail.com>
2026-05-03 16:19:38 +0500

    created frontend resource files

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: '#10B981',
+        secondary: '#3B82F6',
+      },
+    },
+  },
+  plugins: [],
+}
